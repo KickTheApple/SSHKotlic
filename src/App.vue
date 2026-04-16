281471ad22efbd844c42f2bb0676268e7c57ea88
Domenic Dvojmoc <applecakes811@gmail.com>
2026-04-17 00:17:51 +0200

    Frontend init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Menubar from 'primevue/menubar';
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/menu_bar.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+import IconCommunity from "@/components/icons/IconCommunity.vue";
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
